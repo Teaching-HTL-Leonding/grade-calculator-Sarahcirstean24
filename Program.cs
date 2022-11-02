b42131fcc138e5a12e7a310fb1e5b6ba9ee26e0b
Sarahcirstean24 <116262070+Sarahcirstean24@users.noreply.github.com>
2022-11-02 16:43:35 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hast du bei der ersten Prüfung teilgenommen? ja/nein");
+﻿Console.WriteLine("Did you participate the first exam? yes/no");
 string grade = Console.ReadLine()!;
 
 double points = 0;
 double possiblePoints = 0;
 
-if (grade == "ja")
+if (grade == "yes")
 {
-    Console.WriteLine("Wie viele Punkte hast du geschafft? (0-20)");
+    Console.WriteLine("How many points did you get for the first exam (0-20)?");
     points += int.Parse(Console.ReadLine()!);
     possiblePoints += 20;
 }
 
-Console.WriteLine("Hast du bei der zweiten Prüfung teilgenommen? ja/nein");
+Console.WriteLine("Did you participate the second exam? yes/no");
 grade = Console.ReadLine()!;
 
 
-if (grade == "ja")
+if (grade == "yes")
 {
-    Console.WriteLine("Wie viele Punkte hast du geschafft? (0-20) ");
+    Console.WriteLine("How many points did you get for the second exam (0-20)? ");
+    points += int.Parse(Console.ReadLine()!);
+    possiblePoints += 20;
+}
+
+{
+    Console.WriteLine("How many points did you get because of homework (0-20)? ");
     points += int.Parse(Console.ReadLine()!);
     possiblePoints += 20;
 }
@@ -38,11 +43,14 @@ else if (percentage < 76)
 {
     note = 3;
 }
-else if (percentage < 89){
+else if (percentage < 89)
+{
     note = 2;
 }
-if (percentage >= 89)
+else if (percentage >= 89)
 {
     note = 1;
 }
-Console.WriteLine("du bekommst die Note" +note);
+{
+Console.WriteLine("du bekommst die Note" + note);
+}
